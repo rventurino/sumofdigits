bf2cf10cf40c9603fb4d5c9e148625861f881153
Ryan Venturino <ryan.venturino@gmail.com>
2020-12-01 14:55:04 -0500

    v 2.0
    Changed description

--- sumofdigits.js
+++ sumofdigits.js
@@ -1,7 +1,7 @@
 function digital_root(n) {
-  if (n == 0) return n;
+  if (n === 0) return n;
 
   return (n % 10) + digital_root(Math.floor(n / 10));
 }
 
-console.log(digital_root(116));
+console.log(digital_root(456));
